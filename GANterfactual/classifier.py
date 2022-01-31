7027651cc4e0d7bc90f288c766c1554278496693
dimi9595 <dmitry.mindlin@gmail.com>
2022-01-31 23:27:54 +0000

    changed weights path for gpu.

--- GANterfactual/classifier.py
+++ GANterfactual/classifier.py
@@ -11,10 +11,13 @@ import tensorflow as tf
 from GANterfactual.mura_model import WristPredictNet
 from configs.mura_pretraining_config import mura_config
 
+M1_WEIGHTS_PATH = "../checkpoints/mura/best/cp.ckpt"
+GPU_WEIGHTS_PATH = "checkpoints/mura_densenet/best/cp.ckpt"
+
 config = mura_config
 
 def load_classifier():
     model = WristPredictNet(config, train_base=config['train']['train_base'])
     model.built = True
-    model.load_weights("../checkpoints/mura/best/cp.ckpt")
+    model.load_weights(GPU_WEIGHTS_PATH)
     return model
