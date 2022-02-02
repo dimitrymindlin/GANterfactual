46e03f57f6250980bd1e344bec0fc72004e487d0
dimi9595 <dmitry.mindlin@gmail.com>
2022-02-02 13:05:15 +0000

    Changed float formatting.

--- GANterfactual/cyclegan.py
+++ GANterfactual/cyclegan.py
@@ -226,14 +226,14 @@ class CycleGAN():
                 if self.classifier is not None:
                     writer = tf.summary.create_file_writer(f'logs')
                     with writer.as_default():
-                        tf.summary.scalar('D_loss', d_loss[0], step=epoch)
-                        tf.summary.scalar('acc', 100 * d_loss[1], step=epoch)
-                        tf.summary.scalar('G_loss', g_loss[0], step=epoch)
-                        tf.summary.scalar('adv', np.mean(g_loss[1:3]), step=epoch)
-                        tf.summary.scalar('classifier_N', g_loss[3], step=epoch)
-                        tf.summary.scalar('classifier_P', g_loss[4], step=epoch)
-                        tf.summary.scalar('recon', np.mean(g_loss[5:7]), step=epoch)
-                        tf.summary.scalar('id', g_loss[7:9], step=epoch)
+                        tf.summary.scalar('D_loss', tf.reduce_sum(d_loss[0]), step=epoch)
+                        tf.summary.scalar('acc', tf.reduce_sum(100 * d_loss[1]), step=epoch)
+                        tf.summary.scalar('G_loss', tf.reduce_sum(g_loss[0]), step=epoch)
+                        tf.summary.scalar('adv', tf.reduce_sum(np.mean(g_loss[1:3])), step=epoch)
+                        tf.summary.scalar('classifier_N', tf.reduce_sum(g_loss[3]), step=epoch)
+                        tf.summary.scalar('classifier_P', tf.reduce_sum(g_loss[4]), step=epoch)
+                        tf.summary.scalar('recon', tf.reduce_sum(np.mean(g_loss[5:7])), step=epoch)
+                        tf.summary.scalar('id', tf.reduce_sum(g_loss[7:9]), step=epoch)
 
                     progress_str = f"[Epoch: {epoch}/{epochs}] [Batch: {batch_i}] [D_loss: {d_loss[0]:.5f}, acc: {100 * d_loss[1]:.5f}] " \
                                    f"[G_loss: {g_loss[0]:.5f}, adv: {np.mean(g_loss[1:3]):.5f}, classifier_N: {g_loss[3]:.5f}, classifier_P: {g_loss[4]:.5f}, " \
