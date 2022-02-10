c0d2d4506116fe91153591edefc108ca854f2e7d
dimi9595 <dmitry.mindlin@gmail.com>
2022-02-10 16:18:52 +0100

    Added sys argv.

--- scripts/train_cyclegan.py
+++ scripts/train_cyclegan.py
@@ -3,11 +3,17 @@ from __future__ import print_function, division
 import os
 from GANterfactual.cyclegan import CycleGAN
 from configs.gan_training_config import gan_config
+import sys
 
-config = gan_config
+gan_config
+for arg in sys.argv:
+    if arg == "--cycle_consistency":
+        gan_config["train"]["cycle_consistency_loss_weight"] = 8
+    elif arg == "--counterfactual":
+        gan_config["train"]["counterfactual_loss_weight"] = 8
 
 if __name__ == '__main__':
-    gan = CycleGAN()
+    gan = CycleGAN(gan_config)
     gan.construct(classifier_weight=1)
     gan.train()
     gan.save(os.path.join('..', 'models', 'GANterfactual'))
