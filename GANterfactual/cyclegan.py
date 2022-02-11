Added correct image normalisation and correct normal / abnormal pics.

GANterfactual/cyclegan.py
@@ -177,12 +177,13 @@ class CycleGAN():
         class_P = np.stack([np.zeros(batch_size), np.ones(batch_size)]).T
 
         for epoch in range(epochs):
-            for batch_i, (imgs_P, imgs_N) in enumerate(data_loader.load_batch()):
+            for batch_i, (imgs_N, imgs_P) in enumerate(data_loader.load_batch()):
                 # ----------------------
                 #  Train Discriminators
                 # ----------------------i
 
                 # Translate images to opposite domain
+                # P = class label 1, N = class label 0
                 fake_P = self.g_NP.predict(imgs_N)
                 fake_N = self.g_PN.predict(imgs_P)
 
@@ -217,8 +218,11 @@ class CycleGAN():
                     if batch_i % print_interval == 0:
                         with writer.as_default():
                             tf.summary.scalar('D_loss', tf.reduce_sum(d_loss[0]), step=epoch)
+                            # the accuracy of a counterfactual image generator is the percentage of
+                            # counterfactuals that actually changed the classifier’s prediction
                             tf.summary.scalar('acc', tf.reduce_sum(100 * d_loss[1]), step=epoch)
                             tf.summary.scalar('G_loss', tf.reduce_sum(g_loss[0]), step=epoch)
+                            # cycle consistency loss
                             tf.summary.scalar('adv', tf.reduce_sum(np.mean(g_loss[1:3])), step=epoch)
                             tf.summary.scalar('classifier_N', tf.reduce_sum(g_loss[3]), step=epoch)
                             tf.summary.scalar('classifier_P', tf.reduce_sum(g_loss[4]), step=epoch)

GANterfactual/dataloader.py
@@ -20,12 +20,13 @@ class DataLoader():
 
     def load_batch(self):
         for pos, neg in zip(self.dataset.ds_train_pos, self.dataset.ds_train_neg):
-            yield pos[0], neg[0]
+            # pos = class label 1, neg = class label 0
+            yield neg[0], pos[0]  # "NORMAL, ABNORMAL"
 
     def load_single(self):
         self.dataset.ds_test.shuffle(self.dataset.ds_info.splits['test'].num_examples)
-        samples = [(x,y) for x, y in self.dataset.ds_test.take(1)]
-        index = np.random.randint(0,8)
+        samples = [(x, y) for x, y in self.dataset.ds_test.take(1)]
+        index = np.random.randint(0, 8)
         return samples[0][0][index], samples[0][1][index]
 
     def save_single(self, x, path):

data/mura_dataset.py
@@ -57,7 +57,7 @@ class MuraDataset():
         height = self.config['data']['image_height']
         width = self.config['data']['image_width']
         image = tf.image.resize_with_pad(image, height, width)
-        return tf.cast(image, tf.float32) / 255., label  # normalize pixel values
+        return tf.cast(image, tf.float32) / 127.5 - 1., label  # normalize pixel values between -1 and 1
 
     def benchmark(self):
         tfds.benchmark(self.ds_train, batch_size=self.config['train']['batch_size'])