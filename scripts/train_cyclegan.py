8ee604738e04ea087ff847fdb0b6e6132d05240c
dimi9595 <dmitry.mindlin@gmail.com>
2022-02-14 13:41:00 +0100

    Adapted value of losses from paper.

--- scripts/train_cyclegan.py
+++ scripts/train_cyclegan.py
@@ -7,9 +7,9 @@ import sys
 
 for arg in sys.argv:
     if arg == "--cycle_consistency":
-        gan_config["train"]["cycle_consistency_loss_weight"] = 8
+        gan_config["train"]["cycle_consistency_loss_weight"] = 10
     if arg == "--counterfactual":
-        gan_config["train"]["counterfactual_loss_weight"] = 8
+        gan_config["train"]["counterfactual_loss_weight"] = 10
 
 if __name__ == '__main__':
     gan = CycleGAN(gan_config)
