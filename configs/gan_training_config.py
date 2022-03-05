e85eacb2da1284bb32f229460702c66240494d41
dimi9595 <dmitry.mindlin@gmail.com>
2022-03-06 00:53:58 +0100

    updated multiplier.

--- configs/gan_training_config.py
+++ configs/gan_training_config.py
@@ -3,7 +3,7 @@ gan_config = {
         "download": True,
     },
     "data": {
-        "class_names": ["positive"],
+        "class_names": ["negative", "positive"],
         "input_size": (512, 512),
         "image_height": 512,
         "image_width": 512,
@@ -14,7 +14,7 @@ gan_config = {
         "optimizer": "adam",
         "batch_size": 1,
         "learn_rate": 0.0002,
-        "epochs": 15,
+        "epochs": 10,
         "beta1": 0.5,
         "cycle_consistency_loss_weight": 1,
         "identity_loss_weight": 1,
@@ -22,7 +22,7 @@ gan_config = {
         "clf_ckpt": "2022-03-04--16.16",
         "leaky_relu": True,
         "generator": "unet",
-        "generator_training_multiplier": 1
+        "generator_training_multiplier": 2
     },
     "test": {
         "batch_size": 10,
