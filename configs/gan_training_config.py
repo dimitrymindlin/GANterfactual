a0778411f59c42f000f482333c27fa96db5dde63
dimi9595 <dmitry.mindlin@gmail.com>
2022-03-07 12:50:34 +0100

    adapted config

--- configs/gan_training_config.py
+++ configs/gan_training_config.py
@@ -16,13 +16,13 @@ gan_config = {
         "learn_rate": 0.0002,
         "epochs": 10,
         "beta1": 0.5,
-        "cycle_consistency_loss_weight": 5,
+        "cycle_consistency_loss_weight": 1,
         "identity_loss_weight": 1,
         "counterfactual_loss_weight": 1,
         "clf_ckpt": "2022-03-04--16.16",
         "leaky_relu": True,
         "generator": "unet",
-        "generator_training_multiplier": 2
+        "generator_training_multiplier": 1
     },
     "test": {
         "batch_size": 10,
