2eba3dac32ae7283af3241096f10873199968bf6
dimi9595 <dmitry.mindlin@gmail.com>
2022-03-17 09:07:37 +0100

    changed settings according to name.

--- slurm/cyclegan_train_resnet_cycle_consistency.job.sh
+++ slurm/cyclegan_train_resnet_cycle_consistency.job.sh
@@ -3,4 +3,4 @@ source /media/compute/homes/dmindlin/.bashrc
 conda activate GANterfactual
 cd /media/compute/homes/dmindlin/GANterfactual
 
-python3 -m scripts.train_cyclegan --resnet --cycle_consistency --four_times_generator
+python3 -m scripts.train_cyclegan --resnet --cycle_consistency
