8c6de06e8bcd186acde614427643dd3cbf54bcd9
dimi9595 <dmitry.mindlin@gmail.com>
2022-09-16 09:11:06 +0200

    Adapted slurms to this repo.

--- slurm/train_alexNet.sh
+++ slurm/train_alexNet.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source /media/compute/homes/dmindlin/.bashrc
 conda activate GANterfactual
-cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
+cd /media/compute/homes/dmindlin/GANterfactual
 
 python3 -m GANterfactual/train_alexNet
 
