c0a90181d956f9f71fc8191b1eec38a00069e15e
dimi9595 <dmitry.mindlin@gmail.com>
2022-09-23 11:32:38 +0200

    Now training inception. Changed img loading to 3 channel.

--- train_alexNet.py
+++ train_alexNet.py
@@ -1,125 +1,33 @@
 from datetime import datetime
-
 import tensorflow as tf
 import tensorflow.keras as keras
-from tensorflow.keras.layers import BatchNormalization
 import numpy as np
-
+import matplotlib.pyplot as plt
 import os
-
-from tensorflow.python.keras import Input, Model
-from tensorflow.python.keras.layers import Conv2D, Activation, MaxPooling2D, Flatten, Dense, Dropout
-from tensorflow.python.keras.optimizer_v2.gradient_descent import SGD
 from PIL import ImageFile
-from tensorflow.python.keras.regularizers import l2
+
+from GANterfactual.classifier import get_adapted_alexNet
+from GANterfactual.domain_to_domain_model import Domain2DomainModel
 
 TIMESTAMP = datetime.now().strftime("%Y-%m-%d--%H.%M")
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 np.random.seed(1000)
 dimension = 512
-dataset_path = "../tensorflow_datasets/rsna_data"
+if len(tf.config.list_physical_devices('GPU')) == 0:
+    TFDS_PATH = "/Users/dimitrymindlin/tensorflow_datasets/rsna_data"
+else:
+    TFDS_PATH = "../tensorflow_datasets/rsna_data"
 TF_LOG_DIR = 'tensorboard_logs/alexNet/' + TIMESTAMP + "/"
 file_writer = tf.summary.create_file_writer(TF_LOG_DIR)
 with file_writer.as_default():
     tf.summary.text("TS", TIMESTAMP, step=0)
 
 
-def get_adapted_alexNet():
-    input = Input(shape=(dimension, dimension, 1))
-
-    # 1st Convolutional Layer
-    x = Conv2D(filters=96,
-               kernel_size=(11, 11),
-               strides=(4, 4),
-               padding='valid',
-               kernel_regularizer=l2(0.001),
-               bias_regularizer=l2(0.001))(input)
-    x = Activation('relu')(x)
-    # Pooling
-    x = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='valid')(x)
-    # Batch Normalisation before passing it to the next layer
-    x = BatchNormalization()(x)
-
-    # 2nd Convolutional Layer
-    x = Conv2D(filters=256,
-               kernel_size=(11, 11),
-               strides=(1, 1),
-               padding='valid',
-               kernel_regularizer=l2(0.001),
-               bias_regularizer=l2(0.001))(x)
-    x = Activation('relu')(x)
-    # Pooling
-    x = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='valid')(x)
-    # Batch Normalisation
-    x = BatchNormalization()(x)
-
-    # 3rd Convolutional Layer
-    x = Conv2D(filters=384,
-               kernel_size=(3, 3),
-               strides=(1, 1),
-               padding='valid')(x)
-    x = Activation('relu')(x)
-    # Batch Normalisation
-    x = BatchNormalization()(x)
-
-    # 4th Convolutional Layer
-    x = Conv2D(filters=384,
-               kernel_size=(3, 3),
-               strides=(1, 1),
-               padding='valid',
-               kernel_regularizer=l2(0.001),
-               bias_regularizer=l2(0.001))(x)
-    x = Activation('relu')(x)
-    # Batch Normalisation
-    x = BatchNormalization()(x)
-
-    # 5th Convolutional Layer
-    x = Conv2D(filters=256,
-               kernel_size=(3, 3),
-               strides=(1, 1),
-               padding='valid')(x)
-    x = Activation('relu')(x)
-    # Pooling
-    x = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='valid')(x)
-    # Batch Normalisation
-    x = BatchNormalization()(x)
-
-    # Passing it to a dense layer
-    x = Flatten()(x)
-    # 1st Dense Layer
-    x = Dense(4096,
-              kernel_regularizer=l2(0.001),
-              bias_regularizer=l2(0.001))(x)
-    x = Activation('relu')(x)
-    # Add Dropout to prevent overfitting
-    x = Dropout(0.4)(x)
-    # Batch Normalisation
-    x = BatchNormalization()(x)
-
-    # 2nd Dense Layer
-    x = Dense(4096, kernel_regularizer=l2(0.001), bias_regularizer=l2(0.001))(x)
-    x = Activation('relu')(x)
-    # Add Dropout
-    x = Dropout(0.4)(x)
-    # Batch Normalisation
-    x = BatchNormalization()(x)
-
-    # 3rd Dense Layer
-    x = Dense(1000, kernel_regularizer=l2(0.001), bias_regularizer=l2(0.001))(x)
-    x = Activation('relu')(x)
-    # Add Dropout
-    x = Dropout(0.4)(x)
-    # Batch Normalisation
-    x = BatchNormalization()(x)
-    x = Dense(2)(x)
-    x = Activation('softmax')(x)
-
-    opt = SGD(0.0001, 0.9)
-    model = Model(input, x)
-    model.compile(loss='mse',
-                  metrics=['accuracy'],
-                  optimizer=opt)
-    return model
+def plot_any_img(img):
+    if np.min(img) < 0:
+        img = tf.math.add(tf.math.multiply(0.5, img), 0.5)
+    plt.imshow(np.squeeze(img), vmin=np.min(img), vmax=np.max(img), cmap=plt.get_cmap('gray'))
+    plt.show()
 
 
 def get_data():
@@ -129,46 +37,48 @@ def get_data():
     train_gen = keras.preprocessing.image.ImageDataGenerator(preprocessing_function=(lambda x: x / 127.5 - 1.))
 
     train_data = train_gen.flow_from_directory(
-        directory=f"{dataset_path}/train",
+        directory=f"{TFDS_PATH}/train",
         target_size=(image_size, image_size),
         batch_size=batch_size,
         class_mode='categorical',
         shuffle=True,
-        color_mode='grayscale')
+        color_mode='rgb')
 
     validation_data = train_gen.flow_from_directory(
-        directory=f"{dataset_path}/validation",
+        directory=f"{TFDS_PATH}/validation",
         target_size=(image_size, image_size),
         batch_size=batch_size,
         class_mode='categorical',
         shuffle=True,
-        color_mode='grayscale')
+        color_mode='rgb')
 
     test_data = train_gen.flow_from_directory(
-        directory=f"{dataset_path}/test",
+        directory=f"{TFDS_PATH}/test",
         target_size=(image_size, image_size),
         batch_size=batch_size,
         class_mode='categorical',
-        shuffle=True,
-        color_mode='grayscale')
+        shuffle=False,
+        color_mode='rgb')
 
     return train_data, validation_data, test_data
 
 
-model = get_adapted_alexNet()
+# model = get_adapted_alexNet(dimension)
+model = Domain2DomainModel(img_shape=(dimension, dimension, 3)).model()
 # model.summary()
 
 train, validation, test = get_data()
+
 weights_path = f"checkpoints/alexNet/alexNet_{TIMESTAMP}.h5"
 tensorboard_callback = keras.callbacks.TensorBoard(log_dir=TF_LOG_DIR,
-                                    histogram_freq=1,
-                                    write_graph=True,
-                                    write_images=False,
-                                    update_freq='epoch',
-                                    profile_batch=30,
-                                    embeddings_freq=0,
-                                    embeddings_metadata=None
-                                    )
+                                                   histogram_freq=1,
+                                                   write_graph=True,
+                                                   write_images=False,
+                                                   update_freq='epoch',
+                                                   profile_batch=30,
+                                                   embeddings_freq=0,
+                                                   embeddings_metadata=None
+                                                   )
 check_point = keras.callbacks.ModelCheckpoint(weights_path, save_best_only=True, monitor='val_accuracy', mode='max',
                                               save_weights_only=True)
 early_stopping = keras.callbacks.EarlyStopping(min_delta=0.001, patience=10, restore_best_weights=True)
