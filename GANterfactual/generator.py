240d5b62f4cb7067bf4fa5ff012587c48058f0fa
dimi9595 <dmitry.mindlin@gmail.com>
2022-10-12 09:36:40 +0200

    Changed libs to updated tensorflow and keras.

--- GANterfactual/generator.py
+++ GANterfactual/generator.py
@@ -1,9 +1,7 @@
-from keras.layers import Dropout
-from keras.layers import Input, Concatenate
-from keras.layers.advanced_activations import LeakyReLU
-from keras.layers.convolutional import UpSampling2D, Conv2D
-from keras.models import Model
-from keras_contrib.layers.normalization.instancenormalization import InstanceNormalization
+from tensorflow.keras.layers import Dropout, LeakyReLU
+from tensorflow.keras.layers import Input, Concatenate, UpSampling2D, Conv2D
+from tensorflow.keras.models import Model
+from tensorflow_addons.layers import InstanceNormalization
 
 
 def build_generator(img_shape, gf, channels):
