81dccd68e43400603a6914c28d70c3d7f15d7aa9
dimi9595 <dmitry.mindlin@gmail.com>
2022-10-12 13:20:29 +0200

    Added slurms to train GANterfactual.

---
+++ slurm/cyclegan_train_cycle_consistency.job.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+source /media/compute/homes/dmindlin/.bashrc
+conda activate medical-decision-support-system
+cd /media/compute/homes/dmindlin/GANterfactual
+
+python3 -m GANterfactual.cyclegan
