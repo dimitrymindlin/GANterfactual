4aafbebe4d9e50393a4697380af31fefbe2f80f2
dimi9595 <dmitry.mindlin@gmail.com>
2022-10-24 19:01:53 +0200

    Changed to train with inception.

--- scripts/train_cyclegan.py
+++ scripts/train_cyclegan.py
@@ -4,13 +4,16 @@ from datetime import datetime
 
 from GANterfactual.cyclegan import CycleGAN
 
-MODEL_NAME = "alexnet"
+#MODEL_NAME = "alexnet"
+MODEL_NAME = "inception"
+
 if __name__ == '__main__':
     EXECUTION_TS = datetime.now().strftime("%Y-%m-%d--%H.%M")
-    # local_path = "/Users/dimitrymindlin/tensorflow_datasets/rsna_data"
+    #local_path = "/Users/dimitrymindlin/tensorflow_datasets/rsna_data"
     local_path = "../tensorflow_datasets/rsna_data"
     gan = CycleGAN(EXECUTION_TS)
-    gan.construct(classifier_path=f"checkpoints/{MODEL_NAME}_rsna/2022-10-13--13.03/model", classifier_weight=1)
+    #gan.construct(classifier_path=f"checkpoints/{MODEL_NAME}_rsna/2022-10-13--13.03/model", classifier_weight=1)
+    gan.construct(classifier_path=f"checkpoints/{MODEL_NAME}_rsna/2022-10-12--10.37/model", classifier_weight=1)
     gan.train(dataset_name=local_path, epochs=20, batch_size=1, print_interval=10,
               sample_interval=100)
     gan.save(os.path.join('models', f'GANterfactual_{MODEL_NAME}_{EXECUTION_TS}'))
