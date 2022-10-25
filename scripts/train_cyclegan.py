0870499fe7a6b36d9ac3f6d706c3d17ce2d5c0fc
dimi9595 <dmitry.mindlin@gmail.com>
2022-10-25 12:59:13 +0200

    Corrected ckp.

--- scripts/train_cyclegan.py
+++ scripts/train_cyclegan.py
@@ -6,15 +6,16 @@ from GANterfactual.cyclegan import CycleGAN
 
 # MODEL_NAME = "alexnet"
 MODEL_NAME = "inception"
+INCEPTION_CKP = "2022-10-12--10.37"
 
 if __name__ == '__main__':
     EXECUTION_TS = datetime.now().strftime("%Y-%m-%d--%H.%M")
     # local_path = "/Users/dimitrymindlin/tensorflow_datasets/rsna_data"
     local_path = "../tensorflow_datasets/rsna_data"
     gan = CycleGAN(EXECUTION_TS, MODEL_NAME)
-    gan.construct(classifier_path=f"checkpoints/{MODEL_NAME}_rsna/2022-10-13--13.03/model", classifier_weight=1)
-    """gan.construct(
-        classifier_path=f"/Users/dimitrymindlin/UniProjects/GANterfactual/checkpoints/inception_rsna/2022-10-12--10.37/model",
+    gan.construct(classifier_path=f"checkpoints/{MODEL_NAME}_rsna/{INCEPTION_CKP}/model", classifier_weight=1)
+    f"""gan.construct(
+        classifier_path=f"/Users/dimitrymindlin/UniProjects/GANterfactual/checkpoints/{MODEL_NAME}_rsna/{INCEPTION_CKP}/model",
         classifier_weight=1)"""
     gan.train(dataset_name=local_path, epochs=20, batch_size=1, print_interval=10,
               sample_interval=100)
